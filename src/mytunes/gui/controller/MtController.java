13aa063537cfcb8c676d496317b93011d185b81f
Roatze_ <RoatzeTFM@Gmail.com>
2019-11-27 10:01:43 +0100

    Added Controllers

--- src/mytunes/gui/controller/MtController.java
+++ src/mytunes/gui/controller/MtController.java
@@ -10,7 +10,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.media.MediaView;
 
 /**
  *
@@ -19,6 +25,48 @@ import javafx.scene.control.Label;
 public class MtController implements Initializable {
     
     private Label label;
+    @FXML
+    private TableView<?> playlistTable;
+    @FXML
+    private TableView<?> songTable;
+    @FXML
+    private TableView<?> playlistContentTable;
+    @FXML
+    private Button removeSongButton;
+    @FXML
+    private Button closeProgram;
+    @FXML
+    private Button newPlaylistButton;
+    @FXML
+    private Button editPlaylistButton;
+    @FXML
+    private Button deletePlaylistButton;
+    @FXML
+    private Button newSongButton;
+    @FXML
+    private Button editSongButton;
+    @FXML
+    private Button deleteSongButton;
+    @FXML
+    private ToggleButton pauseButton;
+    @FXML
+    private Slider volumeSlider;
+    @FXML
+    private Button nextSong;
+    @FXML
+    private Button previousSong;
+    @FXML
+    private Button transferSongButton;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Label searchLabel;
+    @FXML
+    private TextField searchField;
+    @FXML
+    private MediaView mediaViewMain;
+    @FXML
+    private Label currentSongLabel;
     
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
