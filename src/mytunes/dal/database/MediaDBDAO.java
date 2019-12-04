4aba10c8c177b85550a4b50568a10b694e686c13
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2019-12-04 12:35:44 +0100

    database rettet

--- src/mytunes/dal/database/MediaDBDAO.java
+++ src/mytunes/dal/database/MediaDBDAO.java
@@ -9,8 +9,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import mytunes.be.Media;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import mytunes.be.Media;
 /**
  *
  * @author zilot
@@ -24,18 +25,31 @@ public class MediaDBDAO
         dbCon = new DatabaseConnector();
     }
     
-    public Media createSong(String source, String artist, String title, int time, int year, int category) throws Exception
+ public static void main(String[] args) throws Exception
     {
-        try (Connection con = dbCon.getConnection())
-        {
-            String sql = "INSERT INTO MyTunes_2 VALUES (?,?,?,?,?);";
+        
+        
+            MediaDBDAO mediaDao = new MediaDBDAO();
+          
+            mediaDao.createSong("1.mp3", "Michael Jackson", "Fly Away", 3 * 60 + 34, 1984, 13, 0);
+            
+            
+            
+        }     
+    
+    public Media createSong(String source, String artist, String title, int time, int year, int category, int numplays) throws Exception
+    {
+         Connection con = dbCon.getConnection();
+        
+            String sql = "INSERT INTO songs_2 VALUES (?,?,?,?,?,?,?);";
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1,source);
+            ps.setString(1, source);
             ps.setString(2, artist);
             ps.setString(3, title);
             ps.setInt(4, time);
             ps.setInt(5, year);
             ps.setInt(6, category);
+            ps.setInt(7, numplays);
            int affectedRows = ps.executeUpdate();
             if (affectedRows == 1)
             {
@@ -48,13 +62,8 @@ public class MediaDBDAO
                 }
         
             }
-          
-        
-            
-            
-        } 
+       
               
      return null;
     }
-        
 }
