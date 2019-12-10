ad8678c8e88c146342da1123391b5b2fe3620359
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2019-12-10 09:28:19 +0100

    levet metder til playlist

--- src/mytunes/dal/database/PlaylistDBDAO.java
+++ src/mytunes/dal/database/PlaylistDBDAO.java
@@ -21,41 +21,55 @@ import mytunes.be.Playlist;
  */
 public class PlaylistDBDAO
 {
-    
-     private DatabaseConnector dbCon;
-    
-     public PlaylistDBDAO() throws Exception
+
+    private DatabaseConnector dbCon;
+
+    public PlaylistDBDAO() throws Exception
     {
         dbCon = new DatabaseConnector();
     }
-             
-     //opretter en ny playlist 
-     public Playlist createPlaylist(String name) throws Exception 
-     {
-            Connection con = dbCon.getConnection();
-        
-            String sql = "INSERT INTO playlist VALUES (?);";
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1,name);
-            int affectedRows = ps.executeUpdate();
-            if (affectedRows == 1)
-            {
-                ResultSet rs = ps.getGeneratedKeys();
-                if (rs.next()) 
-                {
-                    int id = rs.getInt(1);
-                    Playlist playlist = new Playlist(id, name);
-                    return playlist;
-                }
-        
-            }
-            return null;
-     }
-             
-    //retunere alle playlister 
-     public List<Playlist> getAllPlaylist() throws Exception 
+
+    public static void main(String[] args) throws Exception
     {
-        try (Connection con = dbCon.getConnection())
+     PlaylistDBDAO playlistdbdao = new PlaylistDBDAO();
+     
+     Playlist playlist = new Playlist( 2 , "hello");
+    
+    // Media meida = new Media(1, "1.mp3", "Michael Jackson", "Fly Away", 3 * 60 + 34, 1983, 13); 
+     
+     System.out.println(playlistdbdao.getAllPlaylist());
+    }
+            
+            
+    
+    
+    //opretter en ny playlist 
+    public Playlist createPlaylist(String name) throws Exception
+    {
+        Connection con = dbCon.getConnection();
+
+        String sql = "INSERT INTO playlist VALUES (?);";
+        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, name);
+        int affectedRows = ps.executeUpdate();
+        if (affectedRows == 1)
+        {
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next())
+            {
+                int id = rs.getInt(1);
+                Playlist playlist = new Playlist(id, name);
+                return playlist;
+            }
+
+        }
+        return null;
+    }
+
+    //retunere alle playlister 
+    public List<Playlist> getAllPlaylist() throws Exception
+    {
+        try ( Connection con = dbCon.getConnection())
         {
             String sql = "SELECT * FROM playlist;";
             Statement statement = con.createStatement();
@@ -63,100 +77,127 @@ public class PlaylistDBDAO
             ArrayList<Playlist> allPlaylist = new ArrayList<>();
             while (rs.next())
             {
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            
-            Playlist playlist = new Playlist( id,  name);
-            allPlaylist.add(playlist);
-            
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+
+                Playlist playlist = new Playlist(id, name);
+                allPlaylist.add(playlist);
+                for (int i = 0; i < allPlaylist.size(); i++)
+            {
+             
+                    List<Media> playlistwithsongs = getPlaylist(allPlaylist.get(i));
+                    
+            }
             }
             return allPlaylist;
-        
+
         }
     }
-    
-     //sletter en playliste 
-      public void deletePlaylist(Playlist playlist) throws Exception
+    // retunere én playliste med dens sange
+    public List<Media> getPlaylist(Playlist playlist) throws Exception
     {
-             Connection con = dbCon.getConnection();
-             int pId = playlist.getId();
+        Connection con = dbCon.getConnection();
+         int p_id = playlist.getId();
 
-             String sqlPt = "DELETE FROM playlist_content_table where playlist_id = (?); ";
-             String sqlP = "DELETE FORM playlist where id=(?);";
-             
-             PreparedStatement ps1 = con.prepareStatement(sqlPt);
-             PreparedStatement ps2 = con.prepareStatement(sqlP);
-
-             ps1.setInt(1, pId);
-             ps2.setInt(1, pId);
-             ps1.executeUpdate();
-             ps2.executeUpdate();
-             ps1.close();
-             ps2.close();
-             
-    }
-      
-      //tilføjer en sang til en playliste 
-     public void addToPlaylist(Playlist playlist, Media media) throws Exception 
-     {
+        String sql = "select s.id, s.source , s.artist, s.title , s.time, s.year, s.category  from songs_2 s, playlist_content_table pc where s.id  = pc.song_id  and pc.playlist_id ="+ p_id +";";
         
-             Connection con = dbCon.getConnection();
-             int pId = playlist.getId();
-             int meId = media.getId();
-             
-             String sql = "insert into playlist_content_table (playlist_id , song_id)values ((?), (?)); ";
-             
-             PreparedStatement pst = con.prepareStatement(sql);
-             
-             pst.setInt(1, pId);
-             pst.setInt(2, meId);
-             
-             pst.executeUpdate();
-             pst.close();
-                     
-             
-             
-     }
-    //fjerner en sang fra en playliste 
-     public void removeFromPlaylist(Playlist playlist, Media media) throws Exception
-     {
-         Connection con = dbCon.getConnection();
-         int pId = playlist.getId();
-         int meId = media.getId();
-         
-         String sql = "Delete From playlist_content_table where playlist_id = (?) and song_id=(?); ";
-         
-         PreparedStatement pst =  con.prepareStatement(sql);
-         
+        Statement ps = con.createStatement();
+        ResultSet rs = ps.executeQuery(sql);
+        ArrayList<Media> playlistWithSongs = new ArrayList<>();
+        while (rs.next())
+        {
+            int id = rs.getInt("id");
+            String source = rs.getString("source");
+            String artist = rs.getString("artist");
+            String title = rs.getString("title");
+            int time = rs.getInt("time");
+            int year = rs.getInt("year");
+            int category = rs.getInt("category");
+
+            Media med = new Media(id, source, artist, title, time, year, category);
+
+            playlistWithSongs.add(med);
+
+        }
+        return playlistWithSongs;
+
+    }
+
+    //sletter en playliste 
+    public void deletePlaylist(Playlist playlist) throws Exception
+    {
+        Connection con = dbCon.getConnection();
+        int pId = playlist.getId();
+
+        String sqlPt = "DELETE FROM playlist_content_table where playlist_id = (?); ";
+        String sqlP = "DELETE FORM playlist where id=(?);";
+
+        PreparedStatement ps1 = con.prepareStatement(sqlPt);
+        PreparedStatement ps2 = con.prepareStatement(sqlP);
+
+        ps1.setInt(1, pId);
+        ps2.setInt(1, pId);
+        ps1.executeUpdate();
+        ps2.executeUpdate();
+        ps1.close();
+        ps2.close();
+
+    }
+
+    //tilføjer en sang til en playliste 
+    public void addToPlaylist(Playlist playlist, Media media) throws Exception
+    {
+
+        Connection con = dbCon.getConnection();
+        int pId = playlist.getId();
+        int meId = media.getId();
+
+        String sql = "insert into playlist_content_table (playlist_id , song_id)values ((?), (?)); ";
+
+        PreparedStatement pst = con.prepareStatement(sql);
+
         pst.setInt(1, pId);
         pst.setInt(2, meId);
-     
+
         pst.executeUpdate();
         pst.close();
-     
-     }
-     
-     //opdatere en playliste med navn 
-     public void updatePlaylist (Playlist playlist, String name) throws Exception 
-     {
-         Connection con = dbCon.getConnection();
-         int pId = playlist.getId();
-         
-         
-         String sql = "Update playlist set name = (?) where playlist_id = (?);";
-         
-        PreparedStatement pst =  con.prepareStatement(sql);
+
+    }
+
+    //fjerner en sang fra en playliste 
+    public void removeFromPlaylist(Playlist playlist, Media media) throws Exception
+    {
+        Connection con = dbCon.getConnection();
+        int pId = playlist.getId();
+        int meId = media.getId();
+
+        String sql = "Delete From playlist_content_table where playlist_id = (?) and song_id=(?); ";
+
+        PreparedStatement pst = con.prepareStatement(sql);
+
+        pst.setInt(1, pId);
+        pst.setInt(2, meId);
+
+        pst.executeUpdate();
+        pst.close();
+
+    }
+
+    //opdatere en playliste med navn 
+    public void updatePlaylist(Playlist playlist, String name) throws Exception
+    {
+        Connection con = dbCon.getConnection();
+        int pId = playlist.getId();
+
+        String sql = "Update playlist set name = (?) where playlist_id = (?);";
+
+        PreparedStatement pst = con.prepareStatement(sql);
         pst.setString(1, name);
         pst.setInt(2, pId);
-        
+
         pst.executeUpdate();
         pst.close();
-        
-     
-     }
-     
-     
-     
-     
-     
+
+    }
+
 }
