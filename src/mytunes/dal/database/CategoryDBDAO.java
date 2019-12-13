cbdb260cd8d6477c8b4d35f1c9c77d77aab10abe
Troels Klein <troels.klein@gmail.com>
2019-12-13 09:43:01 +0100

    Changed the method name a little

--- src/mytunes/dal/database/CategoryDBDAO.java
+++ src/mytunes/dal/database/CategoryDBDAO.java
@@ -11,49 +11,32 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 /**
  *
  * @author zilot
  */
-public class CategoryDBDAO
-{
-    
+public class CategoryDBDAO {
+
     private final DatabaseConnector dbCon;
-    
+
     //connector to DB
-    public CategoryDBDAO() throws Exception
-    {
+    public CategoryDBDAO() throws Exception {
         dbCon = new DatabaseConnector();
     }
-    
 
     //Get a list of all the Categorys 
-     public List<String> getAllCategory() throws Exception 
-    {
-        try (Connection con = dbCon.getConnection())
-        {
+    public List<String> getAllCategories() throws Exception {
+        try (Connection con = dbCon.getConnection()) {
             String sql = "SELECT name FROM CATEGORY;";
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sql);
             List<String> allCategory = new ArrayList<>();
-            while (rs.next())
-            {
-          
-            String name = rs.getString("name");
-            allCategory.add(name);
-            
+            while (rs.next()) {
+                String name = rs.getString("name");
+                allCategory.add(name);
             }
-         
-            return allCategory;
-         }
-    }
-     
-}
-    
-    
-    
-    
-    
 
+            return allCategory;
+        }
+    }
+}
