0b959b1e2d594035a357acb6b281f13e3de1743b
Roatze_ <RoatzeTFM@Gmail.com>
2019-12-13 11:33:15 +0100

    Close Program Button

--- src/mytunes/gui/controller/MtController.java
+++ src/mytunes/gui/controller/MtController.java
@@ -434,4 +434,12 @@ public class MtController implements Initializable
         dataModel.setSelectedPlaylist(playlistsTable.getSelectionModel().getSelectedItem());
         populateSongsInPlaylistList(); // calls setItems again
     }
+
+    @FXML
+    private void CloseProgram(ActionEvent event)
+    {
+        
+        Stage stage = (Stage) closeProgram.getScene().getWindow();
+        stage.close();
+    }
 }
